bff25e78cd2df7494d62f5327ccc2234664674ac
Guadalupe Alejandra Castro Sánchez <91800199+Alecassan@users.noreply.github.com>
2021-12-03 23:58:59 -0600

    Add files via upload

---
+++ equipo-26/Proyecto Final/js/index.js
@@ -0,0 +1,134 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.titulo', {
+    duration: 2000, 
+    origin: 'bottom',
+    
+});
+
+
+sr.reveal('.parrafo1', {
+      duration: 2000, 
+      origin: 'bottom',
+      distance: '-20px'
+});
+
+
+sr.reveal('.parrafo2', {
+    duration: 2000, 
+    origin: 'bottom',
+    distance: '-20px'
+});
+
+
+sr.reveal('.parrafo3', {
+      duration: 2500, 
+      origin: 'bottom',
+      
+});
+
+
+sr.reveal('.col-lg-3', {
+    duration: 2500, 
+    origin: 'right',
+    distance: '-100px'
+    
+});
+
+sr.reveal('.mexpandetitulo', {
+    duration: 2000, 
+      
+});
+
+sr.reveal('.mexpande', {
+    duration: 2500, 
+    origin: 'bottom',
+    distance: '-20px'
+    
+});
+
+sr.reveal('.mexpande2', {
+    duration: 2500, 
+    origin: 'bottom',
+    distance: '-20px'
+    
+});
+
+sr.reveal('.mexpande3', {
+    duration: 2500, 
+    origin: 'bottom',
+    distance: '-20px'
+    
+});
+
+sr.reveal('.mexpande4', {
+    duration: 2000, 
+    origin: 'bottom',
+    distance: '-20px'
+    
+});
+
+sr.reveal('.item', {
+    duration: 2000, 
+    origin: 'left',
+    distance: '-50px'
+    
+});
+
+sr.reveal('.contenedor', {
+    duration: 2000, 
+    origin: 'left',
+    distance: '-50px'
+    
+});
+
+//Pagina de inicio//
+
+sr.reveal('.latina', {
+    duration: 900, 
+    origin: 'bottom',
+    distance: '-8px'
+    
+});
+
+sr.reveal('.eres', {
+    duration: 2000, 
+    origin: 'bottom',
+    distance: '-8px'
+    
+});
+
+sr.reveal('.parini1', {
+    duration: 2000, 
+    origin: 'bottom',
+    distance: '-8px'
+    
+});
+
+sr.reveal('.parini2', {
+    duration: 2000, 
+    origin: 'bottom',
+    distance: '-8px'
+    
+});
+
+sr.reveal('.parini3', {
+    duration: 2000, 
+    origin: 'bottom',
+    distance: '-8px'
+    
+});
+
+sr.reveal('.parini4', {
+    duration: 2000, 
+    origin: 'bottom',
+    distance: '-8px'
+    
+});
+
+sr.reveal('.paisclick', {
+    duration: 2000, 
+    origin: 'left',
+    distance: '20px'
+    
+});
